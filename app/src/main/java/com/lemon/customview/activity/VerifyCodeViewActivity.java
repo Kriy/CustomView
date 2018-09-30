b71c9dcc704644867e6c604aefc03e73abe91b57
dufan <hj2drf@163.com>
2018-09-30 19:56:30 +0800

    添加验证码输入框

---
+++ app/src/main/java/com/lemon/customview/activity/VerifyCodeViewActivity.java
@@ -0,0 +1,17 @@
+package com.lemon.customview.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.lemon.customview.R;
+
+public class VerifyCodeViewActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_verify_code_view);
+    }
+}
