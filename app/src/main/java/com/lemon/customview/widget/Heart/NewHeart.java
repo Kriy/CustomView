6cd26a6865e4bc970c7e41a8da5b88fc907eadc3
dufan <hj2drf@163.com>
2018-10-17 21:50:14 +0800

    no message

---
+++ app/src/main/java/com/lemon/customview/widget/Heart/NewHeart.java
@@ -0,0 +1,11 @@
+package com.lemon.customview.widget.Heart;
+
+public class NewHeart {
+    //坐标点
+    public float x;
+    public float y;
+    //进度（0~1）
+    public float progress;
+    //当前心集合的索引
+    public int index;
+}
