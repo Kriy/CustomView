a7ce3cf7a078afedc2414e2a113a7c4896515141
dufan <hj2drf@163.com>
2018-10-21 22:56:07 +0800

    添加直播间点赞控件

---
+++ app/src/main/java/com/lemon/customview/activity/MeiHeartViewActivity.java
@@ -0,0 +1,74 @@
+package com.lemon.customview.activity;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.SparseArray;
+import android.view.View;
+
+import com.lemon.customview.R;
+import com.lemon.customview.widget.Heart.HeartType;
+import com.lemon.customview.widget.Heart.MeiHeartView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+
+public class MeiHeartViewActivity extends AppCompatActivity {
+
+
+    @BindView(R.id.heart_view)
+    MeiHeartView mHeartView;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mei_heart_view);
+        ButterKnife.bind(this);
+
+
+        setHeartBitmap();
+
+        mHeartView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mHeartView.addHeart();
+            }
+        });
+
+        intervalAddHeart();
+    }
+
+    private void intervalAddHeart() {
+        mHeartView.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                mHeartView.performClick();
+                intervalAddHeart();
+            }
+        }, 1000);
+    }
+
+    public void setHeartBitmap() {
+        SparseArray<Bitmap> bitmapArray = new SparseArray<>();
+        Bitmap bitmap1 = BitmapFactory.decodeResource(this.getResources(), R.mipmap.ic_heart_0);
+        Bitmap bitmap2 = BitmapFactory.decodeResource(this.getResources(), R.mipmap.ic_heart_1);
+        Bitmap bitmap3 = BitmapFactory.decodeResource(this.getResources(), R.mipmap.ic_heart_2);
+        Bitmap bitmap4 = BitmapFactory.decodeResource(this.getResources(), R.mipmap.ic_heart_3);
+        Bitmap bitmap5 = BitmapFactory.decodeResource(this.getResources(), R.mipmap.ic_heart_4);
+        Bitmap bitmap6 = BitmapFactory.decodeResource(this.getResources(), R.mipmap.ic_heart_5);
+        Bitmap bitmap7 = BitmapFactory.decodeResource(this.getResources(), R.mipmap.ic_heart_6);
+        bitmapArray.put(HeartType.BLUE, bitmap1);
+        bitmapArray.put(HeartType.GREEN, bitmap2);
+        bitmapArray.put(HeartType.YELLOW, bitmap3);
+        bitmapArray.put(HeartType.PINK, bitmap4);
+        bitmapArray.put(HeartType.BROWN, bitmap5);
+        bitmapArray.put(HeartType.PURPLE, bitmap6);
+        bitmapArray.put(HeartType.RED, bitmap7);
+        mHeartView.setHeartBitmap(bitmapArray);
+    }
+
+
+}
---
+++ app/src/main/java/com/lemon/customview/widget/Heart/HeartType.java
@@ -0,0 +1,13 @@
+package com.lemon.customview.widget.Heart;
+
+public interface HeartType {
+
+    int BLUE = 0x00;
+    int GREEN = 0x01;
+    int YELLOW = 0x02;
+    int PINK = 0x03;
+    int BROWN = 0x04;
+    int PURPLE = 0x05;
+    int RED = 0x06;
+
+}
