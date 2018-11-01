d90a114b1604617c6d5b1cdaf9a48f45c092f56a
dufan <hj2drf@163.com>
2018-11-01 20:49:31 +0800

    添加web界面

--- app/src/main/java/com/lemon/customview/activity/TheoryActivity.java
+++ app/src/main/java/com/lemon/customview/activity/TheoryActivity.java
@@ -1,5 +1,6 @@
 package com.lemon.customview.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -24,6 +25,9 @@ public class TheoryActivity extends AppCompatActivity {
         switch (view.getId()) {
             case R.id.btn_one:
 //                https://juejin.im/entry/595f0383f265da6c3a54d6bf
+                Intent intent = new Intent(TheoryActivity.this, WebViewActivity.class);
+                intent.putExtra("url", "https://juejin.im/entry/595f0383f265da6c3a54d6bf");
+                startActivity(intent);
                 break;
             case R.id.btn_two:
                 break;
--- app/src/main/java/com/lemon/customview/activity/WebViewActivity.java
+++ app/src/main/java/com/lemon/customview/activity/WebViewActivity.java
@@ -1,14 +1,55 @@
 package com.lemon.customview.activity;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class WebViewActivity extends AppCompatActivity {
+
+    WebView mWebView;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mWebView = new WebView(WebViewActivity.this);
+        setContentView(mWebView);
 
+        String url = getIntent().getStringExtra("url");
+        WebSettings settings = mWebView.getSettings();
+        settings.setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
+        settings.setDatabaseEnabled(true);
+        settings.setAppCacheEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setJavaScriptEnabled(true);
+        settings.setAllowContentAccess(true);
+        settings.setAllowFileAccess(true);
+        settings.setAllowFileAccessFromFileURLs(true);
+        settings.setLoadWithOverviewMode(true);
+        settings.setBuiltInZoomControls(false);
+        if (Build.VERSION.SDK_INT >= 21) {
+            settings.setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
+        }
+
+        mWebView.setWebViewClient(new WebViewClient());
+        mWebView.loadUrl(url);
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (mWebView != null) {
+            ViewGroup parent = (ViewGroup) mWebView.getParent();
+            if (parent != null)
+                parent.removeView(mWebView);
+            mWebView.stopLoading();
+            mWebView.clearHistory();
+            mWebView.removeAllViews();
+            mWebView.destroy();
+        }
+        super.onDestroy();
     }
 }
