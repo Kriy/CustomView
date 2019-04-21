0343dbed4829c3c20c61f7aa4c14d00b265ee1d9
dufan <hj2drf@163.com>
2019-04-21 15:34:18 +0800

    add

---
+++ app/src/main/java/com/lemon/customview/activity/material/ZhiFuBaoActivity.java
@@ -0,0 +1,19 @@
+package com.lemon.customview.activity.material;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.lemon.customview.R;
+
+/**
+ * Created by Terminator on 2019/4/8.
+ */
+public class ZhiFuBaoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_zhifubao);
+    }
+}
